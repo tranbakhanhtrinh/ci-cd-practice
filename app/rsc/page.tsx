e6006b5688a98f0d14fd2ccb8b0163b7ed591e81
tranbakhanhtrinh <tranbakhanhtrinh@gmail.com>
2023-10-28 22:58:47 +0700

    next@13.5.6

--- app/rsc/page.tsx
+++ app/rsc/page.tsx
@@ -1,7 +1,16 @@
-export const metadata = {
-  title: 'App Router'
-}
+import Head from 'next/head'
+
+// export const metadata = {
+//   title: 'App Router'
+// }
 
 export default function Page() {
-  return <h1>App Router</h1>
+  return (
+    <>
+      <Head>
+        <title>App Router</title>
+      </Head>
+      <h1>App Router</h1>
+    </>
+  )
 }
